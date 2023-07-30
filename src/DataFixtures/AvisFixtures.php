f806aacc65f70b16ad3661706e0dd7470e8595e1
wangli <wanglijtm@gmail.com>
2023-07-30 09:28:07 +0200

    ✅ création de projet restaurant + crée l'entity avisAvisClient

---
+++ src/DataFixtures/AvisFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+// class AvisFixtures extends Fixture
+// {
+//     public function load(ObjectManager $manager, Factory $faker): void
+//     {
+//         // $product = new Product();
+//         // $manager->persist($product);
+
+//         $manager->flush();
+//     }
+// }
---
+++ src/Entity/AvisClient.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvisClientRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisClientRepository::class)]
+class AvisClient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $message = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $datePublication = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTimeInterface
+    {
+        return $this->datePublication;
+    }
+
+    public function setDatePublication(\DateTimeInterface $datePublication): static
+    {
+        $this->datePublication = $datePublication;
+
+        return $this;
+    }
+}
