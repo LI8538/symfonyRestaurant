8b9cc5795f450d4cd045442a19b43bd334d3fe27
wangli <wanglijtm@gmail.com>
2023-08-02 11:15:23 +0200

    🐛 réparer le bug de pagination d'avisClient

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,33 @@
 namespace App\Controller;
 
 use App\Repository\ReviewRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ReviewRepository $reviewRepository): Response
-    {
+    public function index(ReviewRepository $reviewRepository, PaginatorInterface $paginator, Request $request): Response
+    {   
+          // On créer une requête pour récupérer les snippets
+          $data = $reviewRepository->findAll();
+
+            $pagination = $paginator->paginate(
+            $data, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, 1 par défaut
+            4 // Nombre de résultats par page
+        );
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'reviews' =>$reviewRepository->findAll(),
+            'reviews' => $pagination
         ]);
+
+        
+
+        
     }
 }
