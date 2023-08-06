5aa74ca43a6545d231a9165ee982103fd502f43c
wangli <wanglijtm@gmail.com>
2023-08-07 00:46:52 +0200

    🐛 corriger le bug de crud Publication

--- src/Form/PublicationType.php
+++ src/Form/PublicationType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\User;
+use App\Entity\Category;
 use App\Entity\Publication;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PublicationType extends AbstractType
@@ -16,8 +19,14 @@ class PublicationType extends AbstractType
             ->add('content')
             ->add('publishedAt')
             ->add('image')
-            ->add('category')
-            ->add('author')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name', // En supposant que 'name' est la propriété dans l'entité Category
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'name', // En supposant que 'name' est la propriété dans l'entité User
+            ])
         ;
     }
 
