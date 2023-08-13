b7a3c883d99d57d55c3a86d92bbc14a8f44d0d57
wangli <wanglijtm@gmail.com>
2023-08-13 21:13:42 +0200

    💄 esyadmin Dashbord refaite et mis à jour

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,11 +2,19 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Dish;
+use App\Entity\User;
+use App\Entity\Drink;
+use App\Entity\Review;
+use App\Entity\Dessert;
+use App\Entity\Starter;
+use App\Entity\Category;
+use App\Entity\Publication;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -26,12 +34,27 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('SymfonyRestaurant');
+            ->setTitle('La table élégance Dashbord');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Gestion');
+        yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Publications', 'fas fa-envelope', Publication::class);
+        yield MenuItem::linkToCrud('Reviews', 'fas fa-comments', Review::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-folder-open', Category::class);
+        //gestion de menu
+        yield MenuItem::section('Gestion la carte');
+        yield MenuItem::linkToCrud('Entrée', 'fas fa-egg', Starter::class);
+        yield MenuItem::linkToCrud('Plats', 'fas fa-utensils', Dish::class);
+        yield MenuItem::linkToCrud('Desserts', 'fas fa-cookie-bite', Dessert::class);
+        yield MenuItem::linkToCrud('Boisson && Cocktail', 'fas fa-champagne-glasses', Drink::class);
+
+        yield MenuItem::section('Autres');
+        yield MenuItem::linkToRoute('Retour à la page d\'accueil', 'fas fa-arrow-left', 'app_home');
+        yield MenuItem::linkToLogout('Déconnexion', 'fas fa-arrow-right-from-bracket');
     }
 }
